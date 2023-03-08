a1b1b74767327a4000df31e908bbfb425677daa3
AaronWong <aaronwonghan@gmail.com>
2023-03-08 04:15:43 -0800

    Refactored projects into subfolders

---
+++ src/JustFeyn.Ui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace JustFeyn.Ui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/JustFeyn.Ui/Pages/Study/VideoStudyPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace JustFeyn.Ui.Pages.Study;
+
+public partial class VideoStudyPage : ContentPage
+{
+	public VideoStudyPage()
+	{
+		InitializeComponent();
+	}
+}
